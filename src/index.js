df893cfd406c19231c5fb465e9ca107680557678
Carson Wright <carsonnwright@gmail.com>
2017-03-17 22:34:34 -0400

    adapter fix

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
+const path = require("path");
 var initStepTest = function (files) {
   (function(window){
-    {{content}}
-
-    var KarmaStepTest = function(_karma){
-      StepTest.on("test.finished", function(test){
-        _karma.result({
-            // test id
-            id: test.id,
-            // test description
-            description: test.name,
-            // an array of string error messages that might explain a failure.
-            // this is required if success is false.
-            log: test.errors,
-            success: test.isSuccess(), // pass / fail
-            skipped: false // skipped / ran
-        })
-      })
-      StepTest.on("finished", function(){
-        _karma.complete()
-      })
-      StepTest.log = function(info){
-        _karma.info(info);
-      }
-      try{
-        StepTest.play()
-      }catch(error){
-        _karma.error(error);
-      }
-    }
-
-  window.__karma__.start = KarmaStepTest;
+    var stepTestPath = path.dirname(require.resolve('step-test'))
   })(typeof window !== 'undefined' ? window : global);
 }
 

d1db24354ce229d1c6dac4534403f5cb0fec2edc
Carson Wright <carsonnwright@gmail.com>
2017-03-17 23:31:33 -0400

    added adapter

---
+++ src/adapter.js
@@ -0,0 +1,30 @@
+(function(window){
+  window.__karma__.start = function(_karma_){
+    StepTest.play();
+    StepTest.on("finished", function(){
+      _karma_.complete();
+    });
+    StepTest.trigger("test.finished", function(test){
+      
+      var result = {
+        description: test.name,
+        id: test.id,
+        log: test.logs,
+        skipped: false,
+        pending: false,
+        success: test.isSuccess(),
+        time: new Date().getTime(),
+        executedExpectationsCount: this.assertions.length
+      };
+      _karma_.result(result);
+    })
+
+     .info({
+      total: data.totalSpecsDefined,
+      specs: getAllSpecNames(jasmineEnv.topSuite())
+    })
+    StepTest.on("finished", function(){
+      _karma_.complete();
+    });
+  }
+})(typeof window ? window : global)

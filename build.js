7853f6aad3d0368ea92d2d6827a075a092524362
Carson Wright <carsonnwright@gmail.com>
2017-03-17 21:15:42 -0400

    fixed karma step test

--- build.js
+++ build.js
@@ -2,10 +2,9 @@ const fs = require("fs");
 let content = fs.readFileSync("./node_modules/step-test/build/step-test.js", "utf8").replace("module.exports", "//");
     content = content.replace(/module.exports/g, "//");
     content += "\n";
-    content += fs.readFileSync("./src/karma-step-test-driver.js", "utf8");
 
 
-let fileWrapper = fs.readFileSync("./src/karma-step-test.wrapper", "utf8");
+let fileWrapper = fs.readFileSync("./src/index.js", "utf8");
 content = fileWrapper.replace("{{content}}", content);
 fs.writeFileSync("./build/karma-step-test.js", content);
 
